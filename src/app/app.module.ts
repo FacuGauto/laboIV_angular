5969a8116b4b31859e99bfa68a85bcd56b60a2b7
FacuGauto <fgauto1992@gmail.com>
2020-08-30 15:52:25 -0300

    agrego clase usuario y componentes bienvenido,login y error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { FormularioComponent } from './formulario/formulario.component';
+import { BienvenidoComponent } from './bienvenido/bienvenido.component';
+import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormularioComponent
+    FormularioComponent,
+    BienvenidoComponent,
+    LoginComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/clases/usuario.ts
@@ -0,0 +1,4 @@
+class Usuario {
+    nombre: String;
+    clave: String;
+  }
